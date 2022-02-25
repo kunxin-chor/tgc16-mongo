11925142a22ddb7d1d63c0c8861beeafc3cf15fa
kunxin-chor <chorkunxin@yahoo.com>
2022-02-25 06:03:20 +0000

    Added create

--- 06-mongo-crud/index.js
+++ 06-mongo-crud/index.js
@@ -13,31 +13,46 @@ app.set('view engine', 'hbs');
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
+// for forms to work
+app.use(express.urlencoded({
+    extended:false
+}));
+
 async function main() {
     // connect to the mongodb
     // first arg of the MongoClient.connect() is the URI (or your connection string)
-    await connect(process.env.MONGO_URI, "sample_airbnb")
+    await connect(process.env.MONGO_URI, "tgc16-food")
 
     // SETUP ROUTES
     app.get('/', async function (req, res) {
-        const data = await getDB().collection('listingsAndReviews') // select the listingsAndReviews collection
-            .find({
-                'beds':{
-                    "$gte":3
-                }
-            },{
-                'projection':{
-                    'name':1,
-                    'description':1,
-                    'beds':1
-                }
-            })
-            .limit(10)
-            .toArray(); // find all documents
+        res.send("show all food records")
+    })
 
-        res.render('listings.hbs',{
-            'listings': data
+    app.get('/food/add', function(req,res){
+        res.render('add_food.hbs',{
+
         })
+    })
+
+    app.post('/food/add', async function(req,res){
+        // step 1. extract info from form
+        let foodName = req.body.foodName;
+        let calories = req.body.calories;
+        let tags = req.body.tags;
+        let tagArray = tags.split(',');
+
+        // step 2. create the new document
+        let foodDocument = {
+            'name': foodName,
+            'calories': calories,
+            'tags': tagArray
+        }
+
+        // step 3. insert in the collection
+        let db = getDB();
+        await db.collection('food_records').insertOne(foodDocument);
+
+        res.send("form recieved");
     })
 }
 
